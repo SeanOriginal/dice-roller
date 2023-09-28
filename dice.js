a097204ae967f2a4774360c9f81596521dda4a0a
SeanOriginal <lewis21sean@gmail.com>
2023-09-27 20:49:43 -0500

    Simplified dice.js scripts

--- dice.js
+++ dice.js
@@ -3,21 +3,23 @@ function diceRoll(num, num2, name1){
     document.getElementById(name1).innerHTML=numberMinMax(num, num2);
 
 }
-function d6() {
-    console.log("button clicked");
-    console.log("this is the d6 dice");
-    numberMinMax(1, 6); // calls numberMinMax function defined below
-    document.getElementById("roll6").innerHTML=numberMinMax(1,6);
-}
-function d10() {
-    console.log("button clicked");
-    console.log("this is the d10 dice");
-    numberMinMax(1, 10); 
-    document.getElementById("roll10").innerHTML=numberMinMax(1,10);
-
-}
 function numberMinMax(min,max) {
     const diceRoll=Math.floor(Math.random() * (max - min +1)) + min;
     console.log(diceRoll);
     return(diceRoll);
 }
+// Indiviualized Functions:
+//
+// function d6() {
+//     console.log("button clicked");
+//     console.log("this is the d6 dice");
+//     numberMinMax(1, 6); // calls numberMinMax function defined below
+//     document.getElementById("roll6").innerHTML=numberMinMax(1,6);
+// }
+// function d10() {
+//     console.log("button clicked");
+//     console.log("this is the d10 dice");
+//     numberMinMax(1, 10); 
+//     document.getElementById("roll10").innerHTML=numberMinMax(1,10);
+
+// }
