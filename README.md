3dcadf75a71391b609e0dfb5ec72721721827f11
SeanOriginal <lewis21sean@gmail.com>
2023-09-27 20:34:44 -0500

    Changes in the main branch

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # dice-roller
 this is a website for dice
 
-User stories
+# User stories
    User creates account
    User logs in
    User rolls dice
+   User results success
 
 User functionalites
      User needs to be able to set up an account
     User needs to be able to login
     User needs to be able to roll dice
+    
--- dice.js
+++ dice.js
@@ -1,18 +1,20 @@
-function diceRoll(num, num2, Name1){
+function diceRoll(num, num2, name1){
     console.log("this is how many sides:", num, num2);
-    document.getElementById(Name1).innerHTML=numberMinMax(num, num2);
+    document.getElementById(name1).innerHTML=numberMinMax(num, num2);
+
 }
 function d6() {
     console.log("button clicked");
     console.log("this is the d6 dice");
     numberMinMax(1, 6); // calls numberMinMax function defined below
-    document.getElementById("roll6").innerHTML=numberMinMax(1,6)
+    document.getElementById("roll6").innerHTML=numberMinMax(1,6);
 }
 function d10() {
     console.log("button clicked");
     console.log("this is the d10 dice");
-    numberMinMax(1, 10); // calls numberMinMax function defined below
-    
+    numberMinMax(1, 10); 
+    document.getElementById("roll10").innerHTML=numberMinMax(1,10);
+
 }
 function numberMinMax(min,max) {
     const diceRoll=Math.floor(Math.random() * (max - min +1)) + min;
